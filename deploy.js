e5f9cbd55e96a9c3fea765224f9dbeb2a4957d8a
Martin Rodriguez <martin@azumo.co>
2018-07-08 00:42:33 -0300

    lottery smart contract lottery test

---
+++ deploy.js
@@ -0,0 +1,19 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+const provider = new HDWalletProvider(
+  "chimney abuse sail custom one bar antique drill version pact mirror never",
+  "https://rinkeby.infura.io/LJAi7Cyw4jt0FxMJNAYK"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account: ', accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: '0x' + bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+  console.log('Contract deployed to: ', result.options.address);
+};
+
+deploy();
